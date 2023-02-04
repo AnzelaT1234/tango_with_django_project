184a761e6ca50ec2d2ed7e63672e35f991d2e7f0
Anzela Tariq <2665724t@student.gla.ac.uk>
2023-02-04 19:37:50 +0000

    Chapter 9

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -97,7 +97,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator', 'OPTIONS':{'min_length':6,}
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
@@ -121,9 +121,19 @@ USE_L10N = True
 
 USE_TZ = True
 
+# Password hashers (from chapter 9.1)
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    'django.contrib.auth.hashers.BCryptPasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+)
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATICFILES_DIRS = [STATIC_DIR, ]
 
 STATIC_URL = '/static/'
+LOGIN_URL = 'rango:login'
+LOGOUT_URL = 'rango:logout'
